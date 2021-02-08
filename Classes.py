ee101d0757bfafb7483e21b34ae3187328b50a2c
Ittussarom Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2021-02-08 14:13:13 -0500

    WIP

---
+++ Classes.py
@@ -0,0 +1,7 @@
+patterns = {}
+class SetupStatement:
+    def __init__(self, pattern, FriendlyName, name):
+        self.pattern = pattern
+        patterns[name]
+        self.FriendlyName = FriendlyName
+        self.name = name
